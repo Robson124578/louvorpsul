6e92790f9b2d208efa878c3ad4cec452de85197d
unknown <robim.net@gmail.com>
2020-09-21 13:37:29 -0300

    Ajustando layout do menu e header

--- index.php
+++ index.php
@@ -7,27 +7,28 @@
     <meta name="keywords" content="Louvor Psul, louvor psul, louvor ebenézer psul, ministerio de louvor voltados para cruz">
     <meta name="author" content="Robson Alves Moreira Pires de Almeida">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&family=Lobster&family=Orbitron&display=swap" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/a4a97c7aaf.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.css">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js" integrity="sha384-LtrjvnR4Twt/qOuYxE721u19sVFLVSA4hf/rRt6PrZTmiPltdZcI7q7PXQBYTKyf" crossorigin="anonymous"></script>
-
     <title>Louvor Psul</title>
 </head>
 <body>
     <div class="container">
         <header class="container-header">
-            <h1><a href="">Louvor Psul</a></h1>
-            <button class="button-menu" id="id-button-menu">menu</button>
+            <a href="#"><h1>Louvor Psul</h1></a>
+            <!-- <button class="button-menu" id="id-button-menu">menu</button> -->
+            <i class="fas fa-bars button-menu" id="id-button-menu"></i>
         </header>
         <nav class="container-menu">
             <ul>
-                <li><a href=""></a>escalas</li>
-                <li><a href=""></a>repertório</li>
-                <li><a href=""></a>sugestões</li>
+            <a href="#"><li>escalas</li></a>
+            <a href="#"><li>repertório</li></a>
+            <a href="#"><li>sugestões</li></a>
             </ul>
         </nav>
     </div>
 </body>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 <script src="js/action.js"></script>
